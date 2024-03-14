6d007a6da26be52b81920142390d6143c65f48ce
SnappyS <thinhnguyentien05@gmail.com>
2024-03-14 17:07:22 +0700

    third mess

---
+++ SNAKE_GITHUB/CHECK/game.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <conio.h>
+#include "myLib.h"
+#include "wall.h"
+#include "snake.h"
+#include "myControl.h"
+
+#define MAX 100
+using namespace std;
+void runGame();
